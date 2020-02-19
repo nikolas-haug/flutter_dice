c0345ab2b012676864207a9ce446c5bbecee2914
Nikolas Haug <nikohaug21@gmail.com>
2020-02-19 14:44:13 -0600

    add 100 dice widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,17 +16,28 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 2;
+
+  var ranNum = 20;
+
   @override
   Widget build(BuildContext context) {
-    var leftDiceNumber = 1;
     return Center(
       child: Row(
         children: <Widget>[
           Expanded(
             child: FlatButton(
               onPressed: () {
-                print('Left button was pressed');
+                setState(() {
+                  leftDiceNumber = Random().nextInt(6) + 1;
+                });
               },
               child: Image.asset('images/dice$leftDiceNumber.png'),
             ),
@@ -33,11 +45,39 @@ class DicePage extends StatelessWidget {
           Expanded(
             child: FlatButton(
               onPressed: () {
-                print('Right button was pressed');
+                setState(() {
+                  rightDiceNumber = Random().nextInt(6) + 1;
+                });
               },
-              child: Image.asset('images/dice5.png'),
+              child: Image.asset('images/dice$rightDiceNumber.png'),
             ),
           ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  ranNum = Random().nextInt(100);
+                });
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.white, width: 10.0),
+                  borderRadius: BorderRadius.all(Radius.circular(3.0)),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(28.0),
+                  child: Text(
+                    ranNum.toString(),
+                    style: TextStyle(
+                      fontSize: 21.0,
+                      color: Colors.yellow[500],
+                      letterSpacing: 1.0,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          )
         ],
       ),
     );
